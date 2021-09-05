2281d32c42dcb0a11f1d28bd5a93d549e21c6f39
Valentine <war.nub@yandex.ru>
2021-09-05 04:00:19 +0300

    some refactoring

--- js/script.js
+++ js/script.js
@@ -12,12 +12,10 @@ window.addEventListener('DOMContentLoaded', () => {
     }
 
     /* services */
-    function createElement(attribute, id, text) {
-        const element = document.createElement('div');
-        element.classList.add('body__item');
-        element.setAttribute(attribute, id);
-        element.innerHTML = `<p class="body__item-text">${text}</p>`;
-        return element;
+    function createNewDiv(...classes) {
+        const div = document.createElement('div');
+        classes.forEach(item => div.classList.add(item));
+        return div;
     }
 
     function hideElements(dataAttribute, itemId) {
@@ -36,7 +34,9 @@ window.addEventListener('DOMContentLoaded', () => {
 
         createItem() {
             if (this.userName) {
-                const item = createElement('data-userId', this.userId, this.userName);
+                const item = createNewDiv('body__item');
+                item.setAttribute('data-userId', this.userId);
+                item.innerHTML = `<p class="body__item-text">${this.userName}</p>`;
 
                 item.addEventListener('click', () => {
                     if (!item.classList.contains('body__item-active')) {
@@ -74,9 +74,10 @@ window.addEventListener('DOMContentLoaded', () => {
         }
 
         createItem() {
-            const item = createElement('data-albumId', this.albumId, this.albumTitle);
-            item.classList.add('body__item-small');
+            const item = createNewDiv('body__item', 'body__item-small');
+            item.setAttribute('data-albumId', this.albumId);
             item.setAttribute('data-parentUser', this.parentUser);
+            item.innerHTML = `<p class="body__item-text">${this.albumTitle}</p>`;
 
             item.addEventListener('click', () => {
                 if (!item.classList.contains('body__item-active')) {
@@ -95,10 +96,10 @@ window.addEventListener('DOMContentLoaded', () => {
     
     /* creating wrapper before showing photos */
     function createAlbumWrapper(parentAlbum, parentUser) {
-        const albumWrapper = document.createElement('div');
-        albumWrapper.classList.add('body__album-wrapper');
+        const albumWrapper = createNewDiv('body__album-wrapper');
         albumWrapper.setAttribute('data-parentAlbum', parentAlbum);
         albumWrapper.setAttribute('data-parentUser', parentUser);
+        
         document.querySelector(`[data-albumId="${parentAlbum}"]`).after(albumWrapper);
     }
 
@@ -126,8 +127,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
         createItem() {
             /* creating photos */
-            const photo = document.createElement('div');
-            photo.classList.add('body__album-item');
+            const photo = createNewDiv('body__album-item');
             photo.setAttribute('data-photoId', this.photoId);
             photo.innerHTML += `<img src="${this.thumbnailUrl}" alt="${this.photoTitle}" width="150px" height="150px">`;
             
@@ -140,35 +140,14 @@ window.addEventListener('DOMContentLoaded', () => {
             });
             
             /* tooltip functional */
-            const tooltip = document.createElement('div');
-            tooltip.classList.add('tooltip');
-            tooltip.classList.add('hide');
+            const tooltip = createNewDiv('tooltip', 'hide');
             tooltip.innerText = `${this.photoTitle}`;
             photo.append(tooltip);
             
-            photo.addEventListener('mouseover', (e) => {
-                if (!e.target.classList.contains('album-item__importance')) {
-                    tooltip.classList.remove('hide');
-                }
-            });
-            
-            photo.addEventListener('mouseout', (e) => {
-                if (!e.target.classList.contains('album-item__importance')) {
-                    tooltip.classList.add('hide');
-                }
-            });
-            
-            photo.addEventListener('mousemove', (e) => {
-                if (!e.target.classList.contains('album-item__importance')) {
-                    tooltip.style.cssText = `top: ${e.clientY + 30}px; left: ${e.clientX - 70}px`; 
-                }
-            });
-            
-            
+            showTooltip(photo, tooltip);
             
             /* creating importance-element */
-            const photoImportance = document.createElement('div');
-            photoImportance.classList.add('album-item__importance');
+            const photoImportance = createNewDiv('album-item__importance');
 
             photoImportance.addEventListener('click', () => {
                 if (!localStorage.getItem(`${this.photoId}`)) {
@@ -181,42 +160,35 @@ window.addEventListener('DOMContentLoaded', () => {
                 showImportance();
             });
             
-            /* add photos to wrapper */
             photo.append(photoImportance);
+            
+            /* add photos to wrapper */
             document.querySelector(`[data-parentAlbum="${this.parentAlbum}"]`).append(photo);
-
         }
     }
     
-    function showTooltip(item, text) {
-        item.addEventListener('mousemove', (e) => {
-            const target = e.target;
-            
-            if (!target.classList.contains('album-item__importance')) {
-                const tooltip = document.createElement('div');
-                tooltip.classList.add('tooltip');
-                tooltip.innerText = `${text}`;
-                item.append(tooltip);
-                
-                // target.addEventListener('mousemove', (e) => {
-                   tooltip.style.cssText = `top: ${e.clientY}px; left: ${e.clientX}px`; 
-                // });
+    function showTooltip(parent, item) {
+        parent.addEventListener('mouseover', (e) => {
+            if (!e.target.classList.contains('album-item__importance')) {
+                item.classList.remove('hide');
             }
-            
         });
-        item.addEventListener('mouseout', (e) => {
-            const target = e.target;
-            if (!target.classList.contains('album-item__importance')) {
-                setTimeout(() => {
-                    document.querySelector('.tooltip').remove();
-                }, 200);
+        
+        parent.addEventListener('mouseout', (e) => {
+            if (!e.target.classList.contains('album-item__importance')) {
+                item.classList.add('hide');
+            }
+        });
+        
+        parent.addEventListener('mousemove', (e) => {
+            if (!e.target.classList.contains('album-item__importance')) {
+                item.style.cssText = `top: ${e.clientY + 30}px; left: ${e.clientX - 70}px`; 
             }
         });
     }
 
     function sizePhoto(url, title) {
-        const modal = document.createElement('div');
-        modal.classList.add('modal');
+        const modal = createNewDiv('modal');
         modal.innerHTML = ` <div class="modal-close"></div>  
                             <div class="modal-content">   
                                 <img src="${url}" alt="${title}">    
@@ -226,8 +198,7 @@ window.addEventListener('DOMContentLoaded', () => {
         document.body.style.overflow = 'hidden';
 
         document.querySelector('.modal').addEventListener('click', (e) => {
-            const target = e.target;
-            if (!target.classList.contains('modal-content')) {
+            if (!e.target.classList.contains('modal-content')) {
                 document.body.style.overflow = '';
                 document.querySelector('.modal').remove();
             }
@@ -268,8 +239,28 @@ window.addEventListener('DOMContentLoaded', () => {
     }
 
     /* main page */
-    const catalog = document.querySelector('#catalog');
+    const catalog = document.querySelector('#catalog'),
+          important = document.querySelector('#important');
+    
+    document.addEventListener('click', (e) => {
+        if (e.target === catalog) {
+            catalog.classList.add('header-menu__item-active');
+            important.classList.remove('header-menu__item-active');
+            
+            showUsers();
+        } else if (e.target === important || e.target.classList.contains('header-menu__important')) {
+            document.querySelectorAll('.body__item').forEach(item => item.remove());
+            
+            important.classList.add('header-menu__item-active');
+            catalog.classList.remove('header-menu__item-active');
+            
+            showFavorite();
+        }
+    });
+    
+    showUsers();
+    
     if (catalog.classList.contains('header-menu__item-active')) {
-        showUsers();
+
     }
 });
